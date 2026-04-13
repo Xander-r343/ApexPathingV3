0ed1518c9ca2c0d7e4037bae4f74fe085319fdc1
Xander Haemel <142128085+Xander-r343@users.noreply.github.com>
2026-04-13 12:20:40 -0700

    Started on Swerve drivetrain, Swerve constants v1 done (#55)
    * vel tuner
    
    * tuners
    
    * constants for swerve
    
    * unfinished swerve skeleton

---
+++ TeamCode/src/main/java/drivetrains/Swerve.java
@@ -0,0 +1,92 @@
+package drivetrains;
+
+import androidx.annotation.NonNull;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import drivetrains.constants.SwerveConstants;
+import motors.MotorEx;
+
+/**
+ * @author Xander Haemel
+ */
+public class Swerve extends Drivetrain{
+    SwerveConstants constants;
+
+    // Motors
+    MotorEx flMotor;
+    MotorEx blMotor;
+    MotorEx frMotor;
+    MotorEx brMotor;
+
+
+    /**
+     * default constructor
+     * @param hardwareMap is the hardwaremap
+     * @param constants: swerveconstants, containing the configuration for your drivetrain
+     */
+    public Swerve(HardwareMap hardwareMap, @NonNull SwerveConstants constants){
+        this.constants = constants;
+        //motors
+        flMotor = new MotorEx(hardwareMap, constants.flData);
+        frMotor = new MotorEx(hardwareMap, constants.frData);
+        blMotor = new MotorEx(hardwareMap, constants.blData);
+        brMotor = new MotorEx(hardwareMap, constants.brData);
+
+    }
+
+    /**
+     * set power based on inputs
+     * @param lfPower is left front power from 0.0 - 0.1
+     * @param lrPower is left rear power from 0.0 - 0.1
+     * @param rfPower is right front power from 0.0 - 0.1
+     * @param rrPower is right rear power from 0.0 - 0.1
+     */
+    private void setPower(double lfPower, double lrPower, double rfPower, double rrPower){
+        double max = Math.max(0, Math.abs(lfPower));
+        max = Math.max(max, Math.abs(lrPower));
+        max = Math.max(max, Math.abs(rfPower));
+        max = Math.max(max, Math.abs(rrPower));
+        if (max > constants.MotorMaxPower) {
+            lfPower = (lfPower / max) * constants.MotorMaxPower;
+            lrPower = (lrPower / max) * constants.MotorMaxPower;
+            rfPower = (rfPower / max) * constants.MotorMaxPower;
+            rrPower = (rrPower / max) * constants.MotorMaxPower;
+        }
+        flMotor.motor.setPower(lfPower);
+        blMotor.motor.setPower(rrPower);
+        frMotor.motor.setPower(rfPower);
+        flMotor.motor.setPower(rrPower);
+    }
+
+    @Override
+    protected void setZeroPowerBehavior(DcMotor.ZeroPowerBehavior behavior) {
+
+    }
+
+    @Override
+    public void moveWithVectors(double drive, double strafe, double turn) {
+
+    }
+
+    @Override
+    public void drive(double x, double y, double turn, double robotHeading) {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void debug(Telemetry telemetry) {
+        telemetry.addData("Front Left Power", flMotor.motor.getPower());
+        telemetry.addData("Front Right Power", frMotor.motor.getPower());
+        telemetry.addData("Back left Power", blMotor.motor.getPower());
+        telemetry.addData("Back Right Power", brMotor.motor.getPower());
+    }
+}
---
+++ TeamCode/src/main/java/drivetrains/constants/SwerveConstants.java
@@ -0,0 +1,199 @@
+package drivetrains.constants;
+
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import motors.MotorMetaData;
+
+/**
+ * @author Xander Haemel 31616
+ * swerve constants file
+ */
+public class SwerveConstants {
+    //motors
+    public MotorMetaData flData = new MotorMetaData("front_left_drive");
+    public MotorMetaData blData = new MotorMetaData("back_left_drive");
+    public MotorMetaData frData = new MotorMetaData("front_right_drive");
+    public MotorMetaData brData = new MotorMetaData("back_right_drive");
+    //servos
+    public String flServoName = "flServo";
+    public String blServoName = "blServo";
+    public String frServoName = "frServo";
+    public String brServoName = "brServo";
+    //encoders
+    public String flEncoderName = "flEncoder";
+    public String blEncoderName = "blEncoder";
+    public String frEncoderName = "frEncoder";
+    public String brEncoderName = "brEncoder";
+
+    //variable constants
+    public double ServoMaxPower = 1;//range 0-1
+    public double MotorMaxPower = 1; //range 0-1
+    public double MaxSpeed = 60; //inches per second
+    public double MotorCurrentLimit = 1000; //milliamps, per motor
+    public double ServoCurrentLimit = 1000; //milliamps per servo
+
+    /**
+     * default constructor
+     */
+    public SwerveConstants() {
+
+    }
+
+    /**
+     * sets the left front servo name
+     * @param name is String name
+     * @return this instance for stacking
+     */
+    public SwerveConstants setFrontLeftServoName(String name){
+        flServoName = name;
+        return this;
+    }
+    /**
+     * sets the left back servo name
+     * @param name is String name
+     * @return this instance for stacking
+     */
+    public SwerveConstants setBackLeftServoName(String name){
+        blServoName = name;
+        return this;
+    }
+    /**
+     * sets the right front servo name
+     * @param name is String name
+     * @return this instance for stacking
+     */
+    public SwerveConstants setFrontRightServoName(String name){
+        frServoName = name;
+        return this;
+    }
+    /**
+     * sets the back right servo name
+     * @param name is String name
+     * @return this instance for stacking
+     */
+    public SwerveConstants setBackRightServoName(String name){
+        brServoName = name;
+        return this;
+    }
+
+    //motors------
+    /**
+     * Sets the left front motor name. Default: "front_left_drive"
+     * @param name the name of the left front motor
+     * @return this instance for chaining
+     */
+    public SwerveConstants setFrontLeftMotorName(String name) {
+        this.flData.setName(name);
+        return this;
+    }
+    /**
+     * Sets the left rear motor name. Default: "back_left_drive"
+     * @param name the name of the left rear motor
+     * @return this instance for chaining
+     */
+    public SwerveConstants setBackLeftMotorName(String name) {
+        this.blData.setName(name);
+        return this;
+    }
+    /**
+     * Sets the right front motor name. Default: "front_right_drive"
+     * @param name the name of the right front motor
+     * @return this instance for chaining
+     */
+    public SwerveConstants setFrontRightMotorName(String name) {
+        this.frData.setName(name);
+        return this;
+    }
+    /**
+     * Sets the right rear motor name. Default: "back_right_drive"
+     * @param name the name of the right rear motor
+     * @return this instance for chaining
+     */
+    public SwerveConstants setBackRightMotorName(String name) {
+        this.brData.setName(name);
+        return this;
+    }
+    //directions
+    /**
+     * Default direction is FORWARD.
+     * @param reversed whether the front left motor is reversed
+     * @return this instance for chaining
+     */
+    public SwerveConstants setFrontLeftReversed(boolean reversed) {
+        this.flData.setDirection(reversed ? DcMotorSimple.Direction.REVERSE : DcMotorSimple.Direction.FORWARD);
+        return this;
+    }
+    /**
+     * Default direction is FORWARD.
+     * @param reversed whether the back left motor is reversed
+     * @return this instance for chaining
+     */
+    public SwerveConstants setBackLeftReversed(boolean reversed) {
+        this.blData.setDirection(reversed ? DcMotorSimple.Direction.REVERSE : DcMotorSimple.Direction.FORWARD);
+        return this;
+    }
+    /**
+     * Default direction is FORWARD.
+     * @param reversed whether the front right motor is reversed
+     * @return this instance for chaining
+     */
+    public SwerveConstants setFrontRightReversed(boolean reversed) {
+        this.frData.setDirection(reversed ? DcMotorSimple.Direction.REVERSE : DcMotorSimple.Direction.FORWARD);
+        return this;
+    }
+    /**
+     * Default direction is FORWARD.
+     * @param reversed whether the back right motor is reversed
+     * @return this instance for chaining
+     */
+    public SwerveConstants setBackRightReversed(boolean reversed) {
+        this.brData.setDirection(reversed ? DcMotorSimple.Direction.REVERSE : DcMotorSimple.Direction.FORWARD);
+        return this;
+    }
+    //other
+
+    /**
+     * sets the max power of the motors on your swerve drivetrain
+     * @param power is a double from 0.0 - 1.0
+     * @return this instance for chaining
+     */
+    public SwerveConstants setMotorMaxPower(double power){
+        this.MotorMaxPower = power;
+        return this;
+    }
+    /**
+     * sets the max power of the servos on your swerve drivetrain
+     * @param power is a double from 0.0 - 1.0
+     * @return this instance for chaining
+     */
+    public SwerveConstants setServoMaxPower(double power){
+        this.ServoMaxPower = power;
+        return this;
+    }
+
+    /**
+     * sets the motor power limit, for each motor
+     * @param milliamps is the limit in milliamps
+     * @return this instance for chaining
+     */
+    public SwerveConstants setMotorCurrentLimit(int milliamps){
+        this.MotorCurrentLimit = milliamps;
+        return this;
+    }
+    /**
+     * sets the servo power limit, for each one
+     * @param milliamps is the limit in milliamps
+     * @return this instance for chaining
+     */
+    public SwerveConstants setServoCurrentLimit(int milliamps){
+        this.ServoCurrentLimit = milliamps;
+        return this;
+    }
+
+
+
+
+
+
+}
